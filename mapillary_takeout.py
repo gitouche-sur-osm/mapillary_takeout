6f4747f402adf90a50300edb7b30c193c2b1f7c1
Gitouche <26656-gitouche@users.noreply.framagit.org>
2020-05-31 11:03:52 +0200

    Code formatting by Black

--- mapillary_takeout.py
+++ mapillary_takeout.py
@@ -10,17 +10,13 @@ import sys
 # mapillary_tools client_id
 CLIENT_ID = "MkJKbDA0bnZuZlcxeTJHTmFqN3g1dzo1YTM0NjRkM2EyZGU5MzBh"
 
-SEQUENCES_PER_PAGE = "100"   # Max from API is 1000, but timeouts
-REQUESTS_PER_CALL = 210      # 220 max
+SEQUENCES_PER_PAGE = "100"  # Max from API is 1000, but timeouts
+REQUESTS_PER_CALL = 210  # 220 max
 SEQUENCE_DL_MAX_RETRIES = 3  # Number of retries for a sequence
 
 API_ENDPOINT = "https://a.mapillary.com"
 
-LOGIN_URL = (
-    API_ENDPOINT
-    + "/v2/ua/login?client_id="
-    + CLIENT_ID
-)
+LOGIN_URL = API_ENDPOINT + "/v2/ua/login?client_id=" + CLIENT_ID
 SEQUENCES_URL = (
     API_ENDPOINT
     + "/v3/sequences?client_id="
@@ -28,28 +24,22 @@ SEQUENCES_URL = (
     + "&per_page="
     + SEQUENCES_PER_PAGE
 )
-MODEL_URL = (
-    API_ENDPOINT
-    + "/v3/model.json?client_id="
-    + CLIENT_ID
-)
+MODEL_URL = API_ENDPOINT + "/v3/model.json?client_id=" + CLIENT_ID
 
 AWS_EXPIRED = '<\?xml version="1\.0" encoding="UTF-8"\?>\\n<Error><Code>AccessDenied<\/Code><Message>Request has expired<\/Message>'
 
 DRY_RUN = False
 
+
 def get_mpy_auth(email, password):
     # Returns mapillary token
     payload = {"email": email, "password": password}
     r = requests.post(LOGIN_URL, json=payload)
-    if 'token' in r.json():
+    if "token" in r.json():
         r.close()
         return r.json()["token"]
-    elif 'message' in r.json():
-        print(
-            'Authentication failed: %s'
-            % r.json()['message']
-        )
+    elif "message" in r.json():
+        print("Authentication failed: %s" % r.json()["message"])
     r.close()
     sys.exit(-1)
 
@@ -65,11 +55,7 @@ def get_user_sequences(mpy_token, username, start_date, end_date):
     r = requests.get(
         SEQUENCES_URL,
         headers=headers,
-        params={
-            'usernames': username,
-            'start_time': start_date,
-            'end_time': end_date
-        }
+        params={"usernames": username, "start_time": start_date, "end_time": end_date},
     )
     for feature in r.json()["features"]:
         response.append(feature)
@@ -80,7 +66,9 @@ def get_user_sequences(mpy_token, username, start_date, end_date):
         r = requests.get(r.links["next"]["url"], headers=headers)
         for feature in r.json()["features"]:
             response.append(feature)
-            nb_images += len(feature["properties"]["coordinateProperties"]["image_keys"])
+            nb_images += len(
+                feature["properties"]["coordinateProperties"]["image_keys"]
+            )
         r.close()
         nb_seq = len(response)
         print("Fetching %s sequences (%s images) ..." % (nb_seq, nb_images))
@@ -97,10 +85,7 @@ def get_source_urls(download_list, mpy_token, username):
     for chunk in chunks:
         params = {
             "paths": json.dumps(
-                [["imageByKey",
-                chunk,
-                ["original_url"]]],
-                separators=(",", ":")
+                [["imageByKey", chunk, ["original_url"]]], separators=(",", ":")
             ),
             "method": "get",
         }
@@ -112,8 +97,10 @@ def get_source_urls(download_list, mpy_token, username):
                 if "value" in image["original_url"]:
                     source_urls[image_key] = image["original_url"]["value"]
                 else:
-                    print("Cant locate original_url for image %r DEBUG %r"
-                          % (image_key, r.text))
+                    print(
+                        "Cant locate original_url for image %r DEBUG %r"
+                        % (image_key, r.text)
+                    )
         r.close()
     return source_urls
 
@@ -125,11 +112,7 @@ def download_sequence(output_folder, mpy_token, sequence, username):
         + sequence["properties"]["created_at"]
     )
     sequence_day = sequence_name.split("T")[0]
-    sorted_folder = (
-        output_folder
-        + '/'
-        + sequence_day
-    )
+    sorted_folder = output_folder + "/" + sequence_day
     download_list = []
     os.makedirs(sorted_folder, exist_ok=True)
 
@@ -137,12 +120,7 @@ def download_sequence(output_folder, mpy_token, sequence, username):
     image_keys = sequence["properties"]["coordinateProperties"]["image_keys"]
     for image_index, image_key in enumerate(image_keys, 1):
         sorted_path = (
-            sorted_folder
-            + "/"
-            + sequence_name
-            + "_"
-            + "%04d" % image_index
-            + ".jpg"
+            sorted_folder + "/" + sequence_name + "_" + "%04d" % image_index + ".jpg"
         )
         if not os.path.exists(sorted_path):
             download_list.append(image_key)
@@ -152,7 +130,7 @@ def download_sequence(output_folder, mpy_token, sequence, username):
 
     print(
         " Already downloaded: %d/%d"
-        % (len(image_keys) - len(download_list),len(image_keys))
+        % (len(image_keys) - len(download_list), len(image_keys))
     )
 
     if DRY_RUN:
@@ -186,59 +164,40 @@ def download_sequence(output_folder, mpy_token, sequence, username):
 
                 if image_key not in source_urls:
                     print(
-                        '  No source url for image %s : refreshing source urls'
+                        "  No source url for image %s : refreshing source urls"
                         % image_key
                     )
-                    source_urls = get_source_urls(
-                        download_list,
-                        mpy_token,
-                        username,
-                    )
+                    source_urls = get_source_urls(download_list, mpy_token, username,)
                     break
 
                 # Get the header first (stream) and compare size
                 try:
                     r = requests.get(source_urls[image_key], stream=True)
                 except requests.exceptions.SSLError:
-                    print(
-                        "  SSL error downloading %r, retrying later"
-                        % image_key
-                    )
+                    print("  SSL error downloading %r, retrying later" % image_key)
                     break
                 except:
                     print(
                         "  Error downloading %r, retrying later. Info %r"
-                        % (
-                            image_key,
-                            sys.exc_info()[0],
-                        )
+                        % (image_key, sys.exc_info()[0],)
                     )
                     break
                 if r.status_code == requests.codes.ok:
-                    size = int(r.headers['content-length'])
-                    if (os.path.isfile(sorted_path) and
-                        os.path.getsize(sorted_path) == size):
-                        print(
-                            "  Already downloaded as %r"
-                            % sorted_path
-                        )
+                    size = int(r.headers["content-length"])
+                    if (
+                        os.path.isfile(sorted_path)
+                        and os.path.getsize(sorted_path) == size
+                    ):
+                        print("  Already downloaded as %r" % sorted_path)
                     else:
                         if os.path.isfile(sorted_path):
-                            print(
-                                "  Size mismatch for %r, replacing..."
-                                % sorted_path
-                            )
+                            print("  Size mismatch for %r, replacing..." % sorted_path)
                         with open(sorted_path, "wb") as f:
                             f.write(r.content)
                     download_list.remove(image_key)
-                elif (r.status_code == 403 and
-                      re.match(AWS_EXPIRED, r.text)):
+                elif r.status_code == 403 and re.match(AWS_EXPIRED, r.text):
                     print(" Download token expired, requesting fresh one ...")
-                    source_urls = get_source_urls(
-                        download_list,
-                        mpy_token,
-                        username,
-                    )
+                    source_urls = get_source_urls(download_list, mpy_token, username,)
                     r.close()
                     # Slow downloads are not an error
                     sequence_dl_retries -= 1
@@ -246,25 +205,16 @@ def download_sequence(output_folder, mpy_token, sequence, username):
                 else:
                     print(
                         "  Error %r downloading image %r : %r"
-                        % (
-                            r.status_code,
-                            image_key,
-                            r.text,
-                        )
+                        % (r.status_code, image_key, r.text,)
                     )
                 r.close()
-    print(
-        " Done downloading sequence %r"
-        % sequence_name
-    )
+    print(" Done downloading sequence %r" % sequence_name)
+
 
 def main(email, password, username, output_folder, start_date, end_date):
     mpy_token = get_mpy_auth(email, password)
     user_sequences, nb_sequences = get_user_sequences(
-        mpy_token,
-        username,
-        start_date,
-        end_date
+        mpy_token, username, start_date, end_date
     )
     for c, sequence in enumerate(reversed(user_sequences), 1):
         print(
@@ -273,7 +223,7 @@ def main(email, password, username, output_folder, start_date, end_date):
                 sequence["properties"]["captured_at"],
                 sequence["properties"]["created_at"],
                 c,
-                nb_sequences
+                nb_sequences,
             )
         )
         download_sequence(output_folder, mpy_token, sequence, username)
@@ -281,40 +231,23 @@ def main(email, password, username, output_folder, start_date, end_date):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Download your images from Mapillary'
+    parser = argparse.ArgumentParser(description="Download your images from Mapillary")
+    parser.add_argument("email", help="Your email address for mapillary authentication")
+    parser.add_argument("password", help="Your mapillary password")
+    parser.add_argument("username", help="Your mapillary username")
+    parser.add_argument("output_folder", help="Download destination")
+    parser.add_argument(
+        "--start-date",
+        help="Filter sequences that are captured since this date",
+        metavar="YYYY-MM-DD",
     )
     parser.add_argument(
-        "email",
-        help='Your email address for mapillary authentication'
+        "--end-date",
+        help="Filter sequences that are captured before this date",
+        metavar="YYYY-MM-DD",
     )
     parser.add_argument(
-        "password",
-        help='Your mapillary password'
-    )
-    parser.add_argument(
-        "username",
-        help='Your mapillary username'
-    )
-    parser.add_argument(
-        "output_folder",
-        help='Download destination'
-    )
-    parser.add_argument(
-        '--start-date',
-        help='Filter sequences that are captured since this date',
-        metavar='YYYY-MM-DD'
-    )
-    parser.add_argument(
-        '--end-date',
-        help='Filter sequences that are captured before this date',
-        metavar='YYYY-MM-DD'
-    )
-    parser.add_argument(
-        "-D",
-        "--dry-run",
-        action="store_true",
-        help="Check sequences status and leave"
+        "-D", "--dry-run", action="store_true", help="Check sequences status and leave"
     )
     args = parser.parse_args()
 
@@ -328,6 +261,6 @@ if __name__ == "__main__":
             args.username,
             args.output_folder,
             args.start_date,
-            args.end_date
+            args.end_date,
         )
     )
