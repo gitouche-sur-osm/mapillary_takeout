89955e01c265c5e80462297af2ba144cb0ccd144
Wolfram Schneider <wosch@freebsd.org>
2020-10-31 16:30:00 +0100

    indent

--- migrate2subfolders.sh
+++ migrate2subfolders.sh
@@ -11,42 +11,44 @@ set -e
 
 # create subfolders first
 create_subfolder ()
-{
-for j in 20??-*
-do 
-(
-  cd $j
-  pwd
-  mkdir -p $(ls | sed -e 's,Z.*,Z,'|sort -u)
-)
-done
+  {
+  for j in 20??-*
+  do 
+  (
+    cd $j
+    pwd
+    mkdir -p $(ls | sed -e 's,Z.*,Z,'|sort -u)
+  )
+  done
 }
 
 # move images to subfolders
 move_images ()
 {
-for j in 20??-*
-do 
-(
-  cd $j
-  if ls | grep -q jpg; then
-    pwd
-    for i in $(ls | grep '\.jpg$' | sed -e 's,Z.*,Z,'| sort -u)
-    do
-      echo $i
-      for k in 0 1 2 3 4 5 6 7 8 9
-      do 
-        if ls | grep -q "${i}.*$k.jpg"; then 
-          printf "."
-          mv ${i}_*${k}.jpg $i
-        fi
+  for j in 20??-*
+  do 
+  (
+    cd $j
+    if ls | grep -q jpg; then
+      pwd
+      for i in $(ls | grep '\.jpg$' | sed -e 's,Z.*,Z,'| sort -u)
+      do
+        echo $i
+        for k in 0 1 2 3 4 5 6 7 8 9
+        do 
+          if ls | grep -q "${i}.*$k.jpg"; then 
+            printf "."
+            mv ${i}_*${k}.jpg $i
+          fi
+        done
+        echo ""
       done
-      echo ""
-    done
-  fi
-)
-done
+    fi
+  )
+  done
 }
 
+create_subfolder
 move_images
 
+#EOF
